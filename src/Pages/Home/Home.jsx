abebfa361a8749766d4806d972f994f8af163d91
Elxan <elxanxanlarov@egmail.com>
2024-11-22 15:06:20 +0400

    cahnge someting

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,16 +12,21 @@ const Home = () => {
             <div className="row g-4">
               <div className="col-md-4 col-sm-6 col-12">
                 <div data-aos="zoom-out-right" className="card p-2">
-                  <img
-                    src="https://media.istockphoto.com/id/880562092/vector/great-britain-united-kingdom-flag.jpg?s=612x612&w=0&k=20&c=z6Qg_O-2pIcreAK3Fb8X003h2HRIFiRWNT8cg7hDfFo="
-                    alt=""
-                  />
-                  <h2>Ingilis Dili</h2>
-                  <p>
-                    Bu kurs sizə ingilis dilində ünsiyyət qurmaq, məqalə oxumaq
-                    və iş yerində daha effektiv olmaq imkanı verəcək. Başlanğıc
-                    və orta səviyyələr üçün uyğundur.
-                  </p>
+                  <div>
+                    
+                    <img
+                      src="https://media.istockphoto.com/id/880562092/vector/great-britain-united-kingdom-flag.jpg?s=612x612&w=0&k=20&c=z6Qg_O-2pIcreAK3Fb8X003h2HRIFiRWNT8cg7hDfFo="
+                      alt=""
+                    />
+                  </div>
+                  <div>
+                    <h2>Ingilis Dili</h2>
+                    <p>
+                      Bu kurs sizə ingilis dilində ünsiyyət qurmaq, məqalə
+                      oxumaq və iş yerində daha effektiv olmaq imkanı verəcək.
+                      Başlanğıc və orta səviyyələr üçün uyğundur.
+                    </p>
+                  </div>
                 </div>
               </div>
               <div className="col-md-4 col-sm-6 col-12">
@@ -45,18 +50,19 @@ const Home = () => {
                 </div>
               </div>
               <div className="col-md-4 col-sm-6 col-12">
-              <div
-                  data-aos="zoom-out-left"
-                  className="card p-2"
-                >
+                <div data-aos="zoom-out-left" className="card p-2">
                   <img
                     src="https://tercuman.az/wp-content/uploads/Alman-diline-tercume.jpg"
                     alt=""
                   />
                   <h2>Alman Dili</h2>
                   <p>
-                Bu kurs sizə alman dilində ünsiyyət qurmaq, əsas dil qrammatikası və lüğətini mənimsəmək imkanı verəcək. Kurs, həm yazılı həm də şifahi ünsiyyət bacarıqlarınızı artırmaq üçün dizayn edilmişdir. Başlanğıc və orta səviyyələr üçün idealdır.
-            </p>
+                    Bu kurs sizə alman dilində ünsiyyət qurmaq, əsas dil
+                    qrammatikası və lüğətini mənimsəmək imkanı verəcək. Kurs,
+                    həm yazılı həm də şifahi ünsiyyət bacarıqlarınızı artırmaq
+                    üçün dizayn edilmişdir. Başlanğıc və orta səviyyələr üçün
+                    idealdır.
+                  </p>
                 </div>
               </div>
             </div>
